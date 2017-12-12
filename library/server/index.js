8d752e263184c5b869b3f1efa9a40ccdfae5849a
Mario Hoyos <mariohoyos92@gmail.com>
2017-12-12 16:38:58 -0600

    feat: add logout redirect

--- library/server/index.js
+++ library/server/index.js
@@ -44,20 +44,23 @@ app.post("/api/auth/register", (req, res, next) => {
 });
 
 app.post("/api/auth/login", (req, res, next) => {
+  console.log(req.body);
   app
     .get("db")
     .checkLogin([req.body.username, req.body.password])
     .then(response => {
-      eq.session.username = response[0].username;
+      console.log(response);
+      req.session.username = response[0].username;
       req.session.ident = response[0].id;
-      res.status(200).json(response[0]);
+      res.status(200).json(req.session);
     })
     .catch(res.status(500));
 });
 
 app.post("/api/auth/logout", (req, res, next) => {
+  console.log(req);
   req.session.destroy();
-  res.status(200);
+  res.status(200).json("logged out");
 });
 
 app.listen(port, () => {
